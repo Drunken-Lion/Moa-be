39127b9e9b2a3ea8f353271d50056271ed4a7292
ChaChaMong <thflf93@gmail.com>
2024-07-11 22:36:07 +0900

    init setting

---
+++ src/main/java/com/moa/moa/MoaBeApplication.java
@@ -0,0 +1,13 @@
+package com.moa.moa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moa/moa/MoaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moa.moa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
