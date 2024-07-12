b32e1feec88a2425ef461ddf8a7b23c1489f3f6e
dc-choi <ddagae0805@gmail.com>
2024-07-12 22:04:17 +0900

    [moa-12] chore: Exception 설정 추가
    Exception 설정을 추가하였습니다.

---
+++ src/main/java/com/moa/moa/global/common/response/ErrorResponse.java
@@ -0,0 +1,37 @@
+package com.moa.moa.global.common.response;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class ErrorResponse {
+    private String httpMethod;
+    private String path;
+    private String message;
+    private LocalDateTime timestamp;
+    private Exception error;
+
+    public static ErrorResponse of(final String message, final HttpServletRequest request) {
+        return ErrorResponse.builder()
+                .httpMethod(request.getMethod())
+                .path(request.getRequestURI())
+                .message(message)
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+
+    public static ErrorResponse of(final String message, final HttpServletRequest request, Exception error) {
+        return ErrorResponse.builder()
+                .httpMethod(request.getMethod())
+                .path(request.getRequestURI())
+                .message(message)
+                .error(error)
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
+}
