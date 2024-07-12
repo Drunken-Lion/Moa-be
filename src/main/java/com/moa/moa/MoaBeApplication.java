407bdcb44bc99cf0d7f527b14c03282db9ef3256
DongChul Choi <ddagae0805@gmail.com>
2024-07-12 21:47:56 +0900

    [moa-8] chore: JpaConfig 추가 (#2)
    QueryDSL 설정과 EnableJpaAuditing 옵션을 전역적으로 사용하기 위한 설정 추가

--- src/main/java/com/moa/moa/MoaBeApplication.java
+++ src/main/java/com/moa/moa/MoaBeApplication.java
@@ -2,12 +2,12 @@ package com.moa.moa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@ConfigurationPropertiesScan
 public class MoaBeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MoaBeApplication.class, args);
     }
