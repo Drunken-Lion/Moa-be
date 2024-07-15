b6a34cc090944612c58f87a52c46958f7d3384c7
DASEUL <77530419+daseul-jang@users.noreply.github.com>
2024-07-15 20:57:09 +0900

    [moa-19] feat: Entity 연관관계 매핑 작업 (#9)
    Question, Place 엔티티에 연관관계 매핑 작업 수행

--- src/main/java/com/moa/moa/api/cs/question/domain/entity/Question.java
+++ src/main/java/com/moa/moa/api/cs/question/domain/entity/Question.java
@@ -1,14 +1,15 @@
 package com.moa.moa.api.cs.question.domain.entity;
 
+import com.moa.moa.api.cs.answer.domain.entity.Answer;
 import com.moa.moa.api.member.member.domain.entity.Member;
 import com.moa.moa.global.common.entity.BaseEntity;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.Comment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -37,4 +38,9 @@ public class Question extends BaseEntity {
     @Comment("문의 상태")
     @Column(name = "status", columnDefinition = "VARCHAR(20)")
     private String status;
+
+    @Comment("문의에 대한 답변")
+    @OneToMany(mappedBy = "question")
+    @Builder.Default
+    private List<Answer> answers = new ArrayList<>();
 }
