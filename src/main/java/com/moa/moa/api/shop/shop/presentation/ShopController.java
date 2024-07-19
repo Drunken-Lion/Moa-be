31b8356a55432061b4b8bfd4404834f839b8302f
Son-Gyeongi <thsruddl77@gmail.com>
2024-07-19 18:14:19 +0900

    [moa-32] feat: Shop Controller Swagger 세팅

---
+++ src/main/java/com/moa/moa/api/shop/shop/presentation/ShopController.java
@@ -0,0 +1,41 @@
+package com.moa.moa.api.shop.shop.presentation;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "v1-shop", description = "렌탈샵 API")
+@RequestMapping("/v1/shops")
+public class ShopController {
+    @Operation(summary = "범위 내 렌탈샵 조회")
+    @GetMapping
+    public ResponseEntity<?> findAllShopWithinRange() {
+
+        return ResponseEntity.ok().body(null);
+    }
+
+    @Operation(summary = "렌탈샵 상세 조회")
+    @GetMapping("{id}")
+    public ResponseEntity<?> findShop(@PathVariable("id") Long id) {
+
+        return ResponseEntity.ok().body(null);
+    }
+
+    @Operation(summary = "최근 본 렌탈샵 조회")
+    @GetMapping("recent")
+    public ResponseEntity<?> findAllShopRecentlyViewed() {
+
+        return ResponseEntity.ok().body(null);
+    }
+
+    @Operation(summary = "최저가 렌탈샵 검색")
+    @PutMapping("search")
+    public ResponseEntity<?> findAllShopSearchForTheLowestPrice() {
+
+        return ResponseEntity.ok().body(null);
+    }
+}
