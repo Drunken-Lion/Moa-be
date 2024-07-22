215216f7c3c864f233f47ec142ec513bdf0b11be
ChaChaMong <thflf93@gmail.com>
2024-07-23 05:21:56 +0900

    [moa-30] feat: place dto 추가

--- src/main/java/com/moa/moa/api/place/place/presentation/PlaceController.java
+++ src/main/java/com/moa/moa/api/place/place/presentation/PlaceController.java
@@ -1,29 +1,32 @@
 package com.moa.moa.api.place.place.presentation;
 
+import com.moa.moa.api.place.place.presentation.dto.FindAllPlaceExternalDto;
+import com.moa.moa.api.place.place.presentation.dto.FindPlaceExternalDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "v1-place", description = "장소 API")
+@Tag(name = "Place-API", description = "장소 API")
 @RequestMapping("/v1/places")
 public class PlaceController {
     @Operation(summary = "스키장 목록 조회")
     @GetMapping
-    public ResponseEntity<?> findAllPlace() {
-
+    public ResponseEntity<List<FindAllPlaceExternalDto.Response>> findAllPlace(@RequestParam(name = "leftTopX") Double leftTopX,
+                                                                               @RequestParam(name = "leftTopY") Double leftTopY,
+                                                                               @RequestParam(name = "rightBottomX") Double rightBottomX,
+                                                                               @RequestParam(name = "rightBottomY") Double rightBottomY) {
         return ResponseEntity.ok().body(null);
     }
 
     @Operation(summary = "스키장 상세 조회")
     @GetMapping("{id}")
-    public ResponseEntity<?> findPlace(@PathVariable("id") Long id) {
+    public ResponseEntity<FindPlaceExternalDto.Response> findPlace(@PathVariable("id") Long id) {
 
         return ResponseEntity.ok().body(null);
     }
---
+++ src/main/java/com/moa/moa/api/place/place/presentation/dto/FindAllPlaceExternalDto.java
@@ -0,0 +1,86 @@
+package com.moa.moa.api.place.place.presentation.dto;
+
+import com.moa.moa.api.place.place.util.enumerated.PlaceLevel;
+import com.moa.moa.api.place.slope.util.enumerated.SlopeLevel;
+import com.moa.moa.api.time.operatingtime.util.enumerated.OperatingType;
+import com.moa.moa.api.time.specificday.util.enumerated.SpecificDayType;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record FindAllPlaceExternalDto() {
+    @Builder
+    public record Response(
+            Long id,
+            String name,
+            LocalDate open,
+            LocalDate close,
+            PlaceLevel recLevel,
+            LocalDateTime createdAt,
+
+            FindAllPlaceExternalDto.ImageResponse image,
+            FindAllPlaceExternalDto.AddressResponse address,
+            List<FindAllPlaceExternalDto.OperatingTimeResponse> operatingTimes,
+            List<FindAllPlaceExternalDto.SpecificDayResponse> specificDays,
+            List<FindAllPlaceExternalDto.AmenityResponse> amenities,
+            List<FindAllPlaceExternalDto.SlopeResponse> slopes
+    ) {
+    }
+
+    @Builder
+    public record ImageResponse(
+            Long id,
+            String originImageUrl,
+            String lowImageUrl,
+            LocalDateTime createdAt) {
+    }
+
+    @Builder
+    public record AddressResponse(
+            Long id,
+            String address,
+            String addressDetail,
+            Double locationX,
+            Double locationY,
+            String mapUrl
+    ) {
+    }
+
+    @Builder
+    public record OperatingTimeResponse(
+            Long id,
+            OperatingType status,
+            String day, // TODO : Type 변경 필요
+            LocalDate open,
+            LocalDate close
+    ) {
+    }
+
+    @Builder
+    public record SpecificDayResponse(
+            Long id,
+            SpecificDayType status,
+            String reason,
+            LocalDate date,
+            LocalDateTime open,
+            LocalDateTime close
+    ) {
+    }
+
+    @Builder
+    public record AmenityResponse(
+            Long id,
+            String name
+    ) {
+    }
+
+    @Builder
+    public record SlopeResponse(
+            Long id,
+            String name,
+            SlopeLevel level
+    ) {
+    }
+}
