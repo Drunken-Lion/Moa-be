05de63ebd02a89b4cbee509a1e6e626e24a1d916
dc-choi <ddagae0805@gmail.com>
2024-07-24 20:07:28 +0900

    [moa-22] feat: InternalException 추가

--- src/main/java/com/moa/moa/global/exception/InternalException.java
+++ src/main/java/com/moa/moa/global/exception/InternalException.java
@@ -1,20 +1,24 @@
 package com.moa.moa.global.exception;
 
 import com.moa.moa.global.common.message.FailHttpMessage;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 /**
- * 400번대 에러를 처리하기 위한 예외 클래스.
+ * 500번대 에러를 처리하기 위한 예외 클래스. <br><br>
+ * Ex) 외부 API 통신 오류 및 Converter 오류
  */
 @Getter
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class BusinessException extends RuntimeException {
+public class InternalException extends RuntimeException {
     private HttpStatus status;
     private String message;
 
-    public BusinessException(FailHttpMessage response) {
+    public InternalException(FailHttpMessage response) {
         super(response.getMessage());
         this.status = response.getStatus();
         this.message = response.getMessage();
