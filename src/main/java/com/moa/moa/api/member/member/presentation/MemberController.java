2dc6affe8e3dbbae2453573b53638e6bc7850a22
daseul-jang <devzzang94@gmail.com>
2024-07-24 18:27:02 +0900

    [moa-28] feat: member external dto 생성
    - dto 생성 및 controller 수정

--- src/main/java/com/moa/moa/api/member/member/presentation/MemberController.java
+++ src/main/java/com/moa/moa/api/member/member/presentation/MemberController.java
@@ -1,23 +1,27 @@
 package com.moa.moa.api.member.member.presentation;
 
+import com.moa.moa.api.member.member.presentation.dto.FindMemberExternalDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.attribute.UserPrincipal;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/members")
-@Tag(name = "v1-member", description = "회원 API")
+@Tag(name = "Member-API", description = "회원 API")
 public class MemberController {
     @Operation(summary = "내 회원 정보 조회")
     @SecurityRequirement(name = "bearerAuth")
     @GetMapping("me")
-    public ResponseEntity<?> findMe() {
+    public ResponseEntity<FindMemberExternalDto.Response> findMe(@AuthenticationPrincipal UserPrincipal user) {
         return ResponseEntity.ok().body(null);
     }
 }
