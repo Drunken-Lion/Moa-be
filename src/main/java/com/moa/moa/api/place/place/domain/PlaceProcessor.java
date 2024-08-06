acf8031d3b12863923eb7f11586136b86a27578e
ChaChaMong <thflf93@gmail.com>
2024-08-07 01:51:25 +0900

    [moa-50] feat: processor interface 삭제

--- src/main/java/com/moa/moa/api/place/place/domain/PlaceProcessor.java
+++ src/main/java/com/moa/moa/api/place/place/domain/PlaceProcessor.java
@@ -1,9 +1,18 @@
 package com.moa.moa.api.place.place.domain;
 
 import com.moa.moa.api.place.place.domain.entity.Place;
+import com.moa.moa.api.place.place.domain.persistence.PlaceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-public interface PlaceProcessor {
-    List<Place> findAllPlaceInMap(Double leftTopX, Double leftTopY, Double rightBottomX, Double rightBottomY);
+@Component
+@RequiredArgsConstructor
+public class PlaceProcessor {
+    private final PlaceRepository placeRepository;
+
+    public List<Place> findAllPlaceInMap(Double leftTopX, Double leftTopY, Double rightBottomX, Double rightBottomY) {
+        return placeRepository.findAllPlaceInMap(leftTopX, leftTopY, rightBottomX, rightBottomY);
+    }
 }
