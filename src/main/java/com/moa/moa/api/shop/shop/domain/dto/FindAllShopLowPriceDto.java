639423f3300d6bd32365519de819722c3bb30e83
Son-Gyeongi <thsruddl77@gmail.com>
2024-09-23 02:07:54 +0900

    [moa-57] chore: 최저가 렌탈샵 검색 시 잘못된 요청으로 에러 나는 경우 방지 - visitDate, liftType, liftTime, packageType 등 null 이 들어오는 거 방지 - 그리고 @Pattern을 통해 필요한 데이터 요청

--- src/main/java/com/moa/moa/api/shop/shop/domain/dto/FindAllShopLowPriceDto.java
+++ src/main/java/com/moa/moa/api/shop/shop/domain/dto/FindAllShopLowPriceDto.java
@@ -6,8 +6,7 @@ import com.moa.moa.api.member.custom.util.enumerated.Gender;
 import com.moa.moa.api.member.custom.util.enumerated.PackageType;
 import com.moa.moa.api.place.place.util.enumerated.PlaceLevel;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 
 import java.math.BigDecimal;
@@ -41,11 +40,12 @@ public record FindAllShopLowPriceDto() {
     public record CustomRequest(
             @NotNull
             Gender gender,
-            @NotBlank
             String nickname,
             @NotBlank
+            @Pattern(regexp = "^(시간지정권-[가-힣]{3}|스마트권)$", message = "'시간지정권-오후권' 또는 '스마트권' 형식으로 입력 가능합니다.")
             String liftType,
             @NotBlank
+            @Pattern(regexp = "^(0?[1-9]|1[0-9]|2[0-4])$", message = "0부터 24까지의 숫자만 입력할 수 있습니다.")
             String liftTime,
             @NotNull
             PackageType packageType,
